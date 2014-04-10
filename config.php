8ccf54b0754af91f6a09db6fb392096113499781
Andre Gardiner <andre@andorum.com>
2014-04-09 19:23:59 -0500

    Update config.php

--- config.php
+++ config.php
@@ -6,14 +6,14 @@ if(!defined('AUTH_CHECK')) die('You are not allowed to execute this file directl
 define('CONSUMER_KEY', '*******');
 define('CONSUMER_SECRET', '*******');
 
-define('OAUTH_CALLBACK', 'twitler/process.php');
+define('OAUTH_CALLBACK', 'process.php');
 
 
 // Database config 
 $db_host		= 'localhost';
 $db_user		= 'root';
 $db_pass		= 'root1';
-$db_database	= 'twitler_db'; 
+$db_database = 'twitler_db'; 
 
 
 $link = mysql_connect($db_host,$db_user,$db_pass) or die('Unable to establish a DB connection');
