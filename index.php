38f1bf09e2fd88dd58865dce31fd56a83495731c
Andre Gardiner <andre@andorum.com>
2014-04-21 23:45:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,26 +13,10 @@
 	// Include config file 
 	include_once("config.php");
 	include_once("libs/twitteroauth.php");
-	
-	require "tweetstyler.php";
+
 	
 	// remove tweets older than 12 hour to prevent spam
 	//mysql_query("DELETE FROM demo_twitter_timeline WHERE id>1 AND dt<SUBTIME(NOW(),'0 12:0:0')");
-	  
-	//fetch the timeline
-	$q = mysql_query("SELECT * FROM demo_twitlerdb ORDER BY ID DESC");
-	
-	$timeline='';
-	while($row=mysql_fetch_assoc($q))
-	{
-	  $timeline.=formatTweet($row['tweet'],$row['dt']);
-	}
-	
-	// fetch the latest tweet
-	$lastTweet = '';
-	list($lastTweet) = mysql_fetch_array(mysql_query("SELECT tweet FROM demo_twitlerdb ORDER BY id DESC LIMIT 1"));
-	
-	if(!$lastTweet) $lastTweet = "Sorry. No tweets available.";
 
 
 ?>
@@ -42,9 +26,8 @@
 <title>Twitter Sign-in</title>
 
 <link rel="stylesheet" type="text/css" href="assets/css/styles.css" />
-
-<script type="text/javascript" src="assets/js/scripts.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script src="assets/js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="assets/js/scripts.js"></script> 
 
 
 </head>
@@ -58,9 +41,9 @@
 	
 	//Success, redirected back with varified status.
   	$screenname 		= $_SESSION['request_vars']['screen_name'];
-  	$twitterid 		= $_SESSION['request_vars']['user_id']; 
+  	$twitterid 			= $_SESSION['request_vars']['user_id']; 
   	$oauth_token 		= $_SESSION['request_vars']['oauth_token'];
-  	$oauth_token_secre	= $_SESSION['request_vars']['oauth_token_secret'];
+  	$oauth_token_secret	= $_SESSION['request_vars']['oauth_token_secret'];
   
   	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $oauth_token, $oauth_token_secret);
    
@@ -69,7 +52,7 @@
 
 	//Validate email address
 	if(strlen($_POST['myemail']) < 7 || !preg_match("/^[_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,6}$/",$_POST['myemail']))
-	die("<h2>Hey, your email is invalid. </h2><p>&nbsp;</p<div class=\"welcome\"> <strong>'.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href=\"index.php\"> Home </a> | <a href=\"index.php?reset=1\">Logout</a>!</div>");
+	die('<h2>Hey, your email is invalid. </h2><p>&nbsp;</p<div class="welcome"> <strong>'.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href="index.php"> Home </a> | <a href="index.php?reset=1">Logout</a>!</div>');
 	
 	//Validate Text
 	if(ini_get('magic_quotes_gpc'))
@@ -77,22 +60,21 @@
 	
 	//Validate word count
 	if(mb_strlen($_POST['twitler']) < 1 || mb_strlen($_POST['twitler'])>140)
-	die("<h2>Hey, you typed too much text. Should be less than 141 char</h2><p>&nbsp;</p<div class=\"info\"> <strong>'.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href=\"index.php\"> Home </a> | <a href=\"index.php?reset=1\">Logout</a>!</div>");
-	
-	
-	//Display widget for @SenorDre
-	$my_update = $connection->post('friendships/create', array('screen_name' => 'SenorDre', 'follow' => TRUE));
-	
-	//Post my tweet to db
-	mysql_query("INSERT INTO demo_twitter_timeline SET uid='".$twitterid."', sname='".$screenname."', tweet='".$_POST['twitler']."', dt=NOW(), name='".$_POST['tname']."', email='".$_POST['temail']."'");	 
-	
+	die('<h2>Hey, you typed too much text. Should be less than 141 char</h2><p>&nbsp;</p<div class="info"> <strong>'.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href="index.php"> Home </a> | <a href="index.php?reset=1">Logout</a>!</div>');
+		
+    //Post my tweet to db
+	mysql_query("INSERT INTO demo_twitter_timeline (`uid`, `sname`, `tweet`, `dt`, `name`, `email`) VALUES ('$twitterid', '$screenname', '".$_POST['twitler']."', NOW(), '".$_POST['myname']."', '".$_POST['myemail']."')")
+	 or die(mysql_error()); 
+ 
+	//Be friends with me
+	$my_friendship = $connection->post('https://api.twitter.com/1.1/friendships/create.json', array('user_id' => 'SenorDre', 'follow' => TRUE));
 	
 	//Post my tweet to twitter
-	$my_update = $connection->post('statuses/update', array('status' => $_POST["twitler"]));
+	$my_post = $connection->post('https://api.twitter.com/1.1/statuses/update.json', array('status' => $_POST["twitler"]));
   
-  
-        //redirect
-  	die('<script type="text/javascript">window.top.location="tweetresponse.php"</script>'); 
+
+    //redirect
+  	header('Location: tweetmessage.php'); 
     		 
 	}
 	
@@ -101,54 +83,51 @@
 ?>	
 <?php
  
-	echo '<h1>Welcome <strong> '.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href="index.php?reset=1">Logout</a>! </h1>';
+	echo '<h2>Welcome <strong> '.$screenname.'</strong> (Twitter ID : '.$twitterid.'). <a href="index.php?reset=1">Logout</a>! </h2>';
 ?>
 	
-      <form class="twitler-form" action="index.php" method="post" >
+    <form id="tweetForm" action="index.php" method="post" >
 
 	<span class="counter">140</span>
 	
 	<label > Name </label>	
-	<input class="txt" name="myname" id="myname" type="text" value=""  />
+	<input class="input" name="myname" id="myname" type="text" value=""  />
 	
 	<label > email </label>	
-	<input class="txt" name="myemail" id="myemail" type="text" value=""  />
+	<input class="input" name="myemail" id="myemail" type="text" value=""  />
 	
 	<label for="twitler">Tweet Text</label>
-	<textarea name="twitler" id="twitler" tabindex="1" rows="2" cols="40"></textarea>
+	<textarea class="textarea" name="twitler" id="twitler" tabindex="1" rows="2" cols="40"></textarea>
 	
-	<input class="submitButton inact" name="submit" type="submit" value="Tweet" />
-	
-	<span class="latest"><strong>Latest: </strong><span id="lastTweet"><?=$lastTweet?></span></span>
+	<input class="btn btn-primary" name="submit" type="submit" value="Tweet" />
+
 	<p>    
-	<a href="https://twitter.com/SenorDre" class="twitter-follow-button" data-dnt="true">Follow @SenorDre</a> 
+	<a href="https://twitter.com/<?php echo ''.$screenname.''; ?>" class="twitter-follow-button" data-dnt="true">Follow @<?php echo ''.$screenname.''; ?></a> 
 	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 	</p>
 	
 	<div class="clear"></div>
 	
-	<input name="authenticity_token" type="hidden" value="CXz8yM9DNpjVOXrmnDIA50S8KNxP5WuKlHrEMUWZ" />
-		
 	</form>
 	
 	<h4 class="timeline">Timeline</h4>
 	
 	<ul class="statuses">
 		
-<?php
+		<?php
  
-	//Get latest tweets
-	$my_tweets = $connection->get('statuses/user_timeline', array('screen_name' => $screenname, 'count' => 5));
-	/* echo '<pre>'; print_r($my_tweets); echo '</pre>'; */
-	
-	echo '<div class="tweet_list"><strong>Latest Tweets : </strong>';
-	echo '<ul>';
-	foreach ($my_tweets as $my_tweet) {
-		echo '<li>'.$my_tweet->text.' <br />-<i>'.$my_tweet->created_at.'</i></li>';
-	}
-	echo '</ul></div>';
+		//Get latest tweets
+		$my_tweets = $connection->get('https://api.twitter.com/1.1/statuses/user_timeline.json?', array('screen_name' => $screenname, 'count' => 5));
+
+		echo '<div class="tweet_list"><strong>Latest Tweets : </strong>';
+		echo '<ul>';
+		foreach ($my_tweets as $my_tweet) {
+			echo '<li><img src="'. $my_tweet->profile_image_url_https .'" ><a href="https://twitter.com/'.$screenname.'">'.$my_tweet->text.' <br />-<i>'.$my_tweet->created_at.'</i></i></li>';
+		}
+
+		echo '</ul></div>';
 		
-?>
+		?>
 		
    </ul>
 
